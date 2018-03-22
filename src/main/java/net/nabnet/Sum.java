f3adaf2e381db331b605b0c0952f4f98479b7568
nabuchi <nabnab.main+github@gmail.com>
2018-03-22 20:55:26 +0900

    16 add times method to Sum

--- src/main/java/net/nabnet/Sum.java
+++ src/main/java/net/nabnet/Sum.java
@@ -10,6 +10,10 @@ class Sum implements Expression {
         this.addend = addend;
     }
 
+    Expression times(int multiplier) {
+        return new Sum(augend.times(multiplier), addend.times(multiplier));
+    }
+
     public Money reduce(Bank bank, String to) {
         int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
         return new Money(amount, to);
